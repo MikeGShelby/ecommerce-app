07ba4a79786b709b47b93085c35c096c0f0502e9
Mike Shelby <mikegshelby@gmail.com>
2021-01-10 15:39:51 -0600

    create ecommerce db to sync to server

--- config/connection.js
+++ config/connection.js
@@ -11,11 +11,11 @@ if (process.env.JAWSDB_URL) {
 } else {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
-    port: 3306,
     dialect: 'mysql',
     dialectOptions: {
       decimalNumbers: true,
-    }
+    },
+    port: 3306
   });
 }
 
