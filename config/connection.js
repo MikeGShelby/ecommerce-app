f3adad35e569e426538a85b8d81ad842946c6314
Mike Shelby <mikegshelby@gmail.com>
2021-01-10 13:56:10 -0600

    initial modifications to project directory

--- config/connection.js
+++ config/connection.js
@@ -3,14 +3,20 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+// create connection to ecommerce_db
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    port: 3306,
+    dialect: 'mysql',
+    dialectOptions: {
+      decimalNumbers: true,
+    }
+  });
+}
 
 module.exports = sequelize;
